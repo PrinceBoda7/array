59a8764fd597e3f48f938890a1bd84bc2e097d16
PrinceBoda7 <162782524+PrinceBoda7@users.noreply.github.com>
2024-03-09 12:23:50 +0530

    Add files via upload

---
+++ Lab44.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Lab44{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = {76,89,65,98,96};
+		int i = 0;
+		for(i=arr.length-1;i>=0;i--){
+			System.out.println(arr[i]);
+		}
+	}
+}
